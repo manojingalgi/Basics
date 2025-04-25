3a3599eeb60235c9cd1e6a159bc493aab2eea70e
manojingalgi <manojingalgi@gmail.com>
2025-04-25 13:46:35 +0530

    conditional prgms

--- .venv/Day1/conditionalStatements.py
+++ .venv/Day1/conditionalStatements.py
@@ -26,4 +26,62 @@
 # else:
 #     print("year {} is not a leap year".format(year))
 
+#TYPE SINGLE LINE
+# num =10192929023429352945
+# print("even") if num%2==0 else print("odd")
+
+#multiple print statements
+# a=20
+# {print("hi"),print("python")} if a>10 else {print("hello"),print("java")}
+
+
+# #elif
+# weekday =10
 #
+# if weekday==1 :
+#     print("monday")
+# elif weekday ==2 :
+#     print("tuesday")
+# elif weekday ==3 :
+#     print("wednsday")
+# elif weekday ==4 :
+#     print("thrusday")
+# elif weekday ==5 :
+#     print("friday")
+# elif weekday ==6 :
+#     print("saturday")
+# elif weekday ==7 :
+#     print("sunday")
+# else :
+#     print("invalid number")
+
+#
+# #ASSIGNEMTS
+# num = 10
+# if num < 0:
+#     print("negative number")
+# else :
+#     print("Positive number")
+#
+# print("###############################################")
+# num1 =10
+# num2 =20
+# print("num1 is largest")if num1>num2 else print("num2 is larggest")
+#
+# print("########################################################")
+#
+# a=10
+# b=200
+# c=3
+#
+# if a>b and a>c :
+#     print("{} is largest".format(a))
+# elif b<a and b>c :
+#     print("{} is largest".format(b))
+# else :
+#     print ("{} is largest".format(c))
+#
+#
+
+
+
