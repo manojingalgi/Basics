15d6d5db7392aa53041c9b6ce4562baff5091237
manojingalgi <manojingalgi@gmail.com>
2025-04-27 18:11:05 +0530

    looping concept

---
+++ .venv/Day2/looping.py
@@ -0,0 +1,76 @@
+# #Take a number and print values from N to 0
+# n=20
+# while n>=1:
+#     print(n)
+#     n=n-1
+#
+from math import factorial
+
+# Sum of Digits
+# Calculate the sum of digits of a number (e.g., 12345 → 15).
+# n=12345
+# sum =0
+# while n>0:
+#     rem = n%10
+#     sum =sum+ rem
+#     print(sum)
+#     n =n//10
+
+#Print first 10 even numbers
+# n=0
+# count =0
+#
+# while count<=10:
+#     n=n+2
+#     print(n)
+#     count=count+1
+# print("done")
+#
+#Sum of digits of a number
+# n=54321
+#
+# tot =0
+# while n>0:
+#     digit = n%10
+#     tot = tot+digit
+#     print(tot)
+#     n=n//10
+# print("done")
+
+#factorial
+# n =6
+# fact=1
+# while n>1:
+#     fact =fact *(n-1)
+#     n=n-1
+# print(fact)
+
+
+#Palindrome checker
+# num = -1221
+# temp = num
+# reverse = 0
+# while temp > 0:
+#     remainder = temp % 10
+#     reverse = (reverse * 10) + remainder
+#     temp = temp // 10
+# if num == reverse:
+#   print('Palindrome')
+# else:
+#   print("Not Palindrome")
+#
+
+# #FORLOOP
+# for i in range(10,0,-1):
+#     print(i)
+
+
+#even numbers
+#
+# for i in range (0,21,2):
+#     print(i)
+
+#decensing order:
+
+for i in range(10,0,-1):
+    print(i)
---
+++ .venv/Day2/rangefunctions.py
@@ -0,0 +1,13 @@
+#range() -> values between the range
+
+print(list(range(10)))   #gives list =>[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(list(range(5,10))) #=> [5, 6, 7, 8, 9]
+
+print(list(range(1,50,5)))   #=>[1, 6, 11, 16, 21, 26, 31, 36, 41, 46]
+
+
+print(range(1,50,5)) #=> range(1, 50, 5)
+
+print(list(range(-10,-5)))  #=> [-10, -9, -8, -7, -6]
+print(list(range(-20,10,2)))  #= > [-20, -18, -16, -14, -12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8]
