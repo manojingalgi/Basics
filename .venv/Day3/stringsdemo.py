ef86bf5166ba10351eb3198ca82381b3ecc6bed8
manojingalgi <manojingalgi@gmail.com>
2025-04-29 12:12:00 +0530

    day3 and 4 files

---
+++ .venv/Day3/stringsdemo.py
@@ -0,0 +1,38 @@
+# #ways of creating string
+# s="manoj"
+# s='manoj'
+# s=str('manoj')
+#
+# #empty string
+# s =""
+# s=''
+# s= str('')
+# s=str()
+
+
+# str="welcome"
+# print(str[1:3]) # el
+# print(str[:6])  #welcom
+# print(str[2:]) #lcome
+
+
+#s = "welcome to python"
+# print(s.endswith("thon"))
+# print(s.startswith("to"))
+# print(s.find("come"))
+#print(s.replace("to","in"))
+
+#reverse string
+#method 1
+# s="welcome"
+# rev= ""
+# for i in s:
+#     rev = i+rev
+# print(rev)
+#
+# #method 2
+# s="welcome"
+# rev=s[::-1]
+# print(rev)
+
+
---
+++ .venv/Day4/listsdemo.py
@@ -0,0 +1,6 @@
+#exp1 of litsts
+mylists1=[10,20,30,40,50]
+mylists2 = ["apple","mango","orange"]
+mylists3 =[1,2,"aaple",8,"blore"]
+mylists4 =list()
+
