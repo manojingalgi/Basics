e9ad70c1e02bcbeee17572140b02e154a713cdd5
manojingalgi <manojingalgi@gmail.com>
2025-04-30 13:53:03 +0530

    day4 dictonary

---
+++ .venv/Day4/dictdemo.py
@@ -0,0 +1,32 @@
+newdict = {101:"x", 102:'y',103:'z'}
+
+dict1 = {
+    "brand" :"tata",
+    "model": "nexon",
+    "year" : 2025
+}
+print(dict1["brand"])
+
+# dict1["year"] = 2022
+# print(dict1["year"])
+#
+# for i in dict1 :
+#     print(i) #retunns only key
+#
+# for i in dict1:
+#     print(dict1[i]) #only values
+#
+# for x,y in dict1.items():
+#     print(x,y)  #displays both key and value
+#
+
+#check item preset or not
+#
+# if "model" in dict1:
+#     print("exists")
+# else:
+#     print("not exists")
+#
+#
+dict1["colour"] = "safforn"
+print(dict1)
